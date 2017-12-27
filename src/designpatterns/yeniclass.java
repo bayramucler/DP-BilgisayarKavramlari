4e1574b63280294af33f79a5516b674c29cc4460
bayramucler <bayramucler@gmail.com>
2017-12-27 21:26:22 +0300

    ikinci acıklama satırı eklendi

--- src/designpatterns/yeniclass.java
+++ src/designpatterns/yeniclass.java
@@ -14,4 +14,5 @@ public class yeniclass {
     public static void main(String args[]){
        System.out.println("deneme");
         //bu açıklama satırı github üzerinden eklendi
+        //bu ikinci açıklama satırı da github üzerinden eklendi. ardından git bash ile pull edildi.
 }}
