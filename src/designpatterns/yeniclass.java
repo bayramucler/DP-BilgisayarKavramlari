fe304719c61a5f253cda29972d1c6332e4d04af0
Bayram Üçler <bayramucler@gmail.com>
2017-12-27 21:27:26 +0300

    modified

--- src/designpatterns/yeniclass.java
+++ src/designpatterns/yeniclass.java
@@ -12,6 +12,7 @@ package designpatterns;
 public class yeniclass {
     
     public static void main(String args[]){
+        
        System.out.println("deneme");
         //bu açıklama satırı github üzerinden eklendi
         //bu ikinci açıklama satırı da github üzerinden eklendi. ardından git bash ile pull edildi.
